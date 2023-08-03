22afbc7d3391cfa9a666112bee2c629d6737994f
dimoka777 <dimokavip777@gmail.com>
2023-08-03 17:46:14 +0200

    add: 03.08.2023

---
+++ 1853. Convert Date Format.sql
@@ -0,0 +1,18 @@
+# Table: Days
+#
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | day         | date |
+# +-------------+------+
+# day is the primary key for this table.
+#
+#
+# Write an SQL query to convert each date in Days into a string formatted as "day_name, month_name day, year".
+#
+# Return the result table in any order.
+#
+# The query result format is in the following exemple.
+
+
+# SELECT DATE_FORMAT(day, "%W, %M %e, %Y") as day FROM Days
