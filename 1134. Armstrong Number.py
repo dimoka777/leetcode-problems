8e1ca223b4357416379e923aef244ce395fb39e4
dimoka777 <dimokavip777@gmail.com>
2023-08-04 22:25:30 +0200

    add: 04.08.2023

---
+++ 1134. Armstrong Number.py
@@ -0,0 +1,13 @@
+# Given an integer n, return true if and only if it is an Armstrong number.
+#
+# The k-digit number n is an Armstrong number if and only if the kth power of each digit sums to n.
+#
+# Example 1:
+#
+# Input: n = 153
+# Output: true
+# Explanation: 153 is a 3-digit number, and 153 = 13 + 53 + 33.
+
+class Solution:
+    def isArmstrong(self, n: int) -> bool:
+        return sum([int(i) ** len(str(n)) for i in str(n)]) == n
