1da3b6e80bfb77b7a448528aa11f5a24b26149a4
dimoka777 <dimokavip777@gmail.com>
2023-08-06 16:51:58 +0200

    add: 06.08.2023

--- 183. Customers Who Never Order.py
+++ 183. Customers Who Never Order.py
@@ -59,5 +59,4 @@ import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     df = customers[~customers['id'].isin(orders['customerId'])]
-    df.rename(columns={'name': 'Customers'}, inplace=True)
-    return df[['Customers']]
+    return df[['name']].rename(columns={'name': 'Customers'})
