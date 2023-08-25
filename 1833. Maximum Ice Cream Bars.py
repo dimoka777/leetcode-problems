178a9049ed623cd822a1427daca8e2cc2d65d41b
Dilmurat Aliev <51176763+dimoka777@users.noreply.github.com>
2023-08-25 03:19:04 +0300

    add: 25.08.2023

---
+++ 1833. Maximum Ice Cream Bars.py
@@ -0,0 +1,27 @@
+# It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+
+# At the store, there are n ice cream bars. 
+# You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins.
+# The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+# Note: The boy can buy the ice cream bars in any order.
+# Return the maximum number of ice cream bars the boy can buy with coins coins.
+# You must solve the problem by counting sort.
+
+# Example 1:
+
+# Input: costs = [1,3,2,4,1], coins = 7
+# Output: 4
+# Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
+
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        res = 0
+        for i in costs:
+            coins -= i
+            if coins < 0:
+                return res
+            res += 1
+            
+        return res
