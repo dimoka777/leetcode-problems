5704da64a3731e5b29ec188cb901058c518d0ab8
Dilmurat Aliev <51176763+dimoka777@users.noreply.github.com>
2023-10-30 15:30:53 +0300

    744. Find Smallest Letter Greater Than Target

---
+++ 744. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,29 @@
+# Easy
+# Amazon LinkedIn Bloomberg
+# You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+# Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+# Example 1:
+
+# Input: letters = ["c","f","j"], target = "a"
+# Output: "c"
+# Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left = 0
+        right = len(letters) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        if left == len(letters):
+            return letters[0]
+        else:
+            return letters[left]
